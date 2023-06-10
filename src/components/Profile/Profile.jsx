9b83928486fe0f4ae33acc4a90e620b1f2e610f7
MateuszKar2 <system505@protonmail.com>
2023-06-10 19:58:03 +0200

    add file

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,34 +1,42 @@
-import PropTypes from "prop-types";
 import css from './Profile.module.css';
+import PropTypes from "prop-types";
+import React from 'react';
 
-const Profile = ({username, tag, location, avata, stats}) => {
-  return (
-<div className="profile">
-  <div class="description">
+export const Profile = ({username, tag, location, avatar, stats}) => {
+
+<div className={css.profile}>
+  <div class={css.description}>
     <img
-      src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-      alt="User avatar"
-      class="avatar"
+      src={avatar}
+      alt={username}
+      class={css.avatar}
     />
-    <p class="name">Petra Marica</p>
-    <p class="tag">@pmarica</p>
-    <p class="location">Salvador, Brasil</p>
+    <p class={css.name}>Petra Marica</p>
+    <p class={css.tag}>@pmarica</p>
+    <p class={css.location}>Salvador, Brasil</p>
   </div>
 
-  <ul class="stats">
+  <ul class={css.stats}>
     <li>
-      <span class="label">Followers</span>
-      <span class="quantity">1000</span>
+      <span class={css.label}>Followers</span>
+      <span class={css.quantity}>1000</span>
     </li>
     <li>
-      <span class="label">Views</span>
-      <span class="quantity">2000</span>
+      <span class={css.label}>Views</span>
+      <span class={css.quantity}>2000</span>
     </li>
     <li>
-      <span class="label">Likes</span>
-      <span class="quantity">3000</span>
+      <span class={css.label}>Likes</span>
+      <span class={css.quantity}>3000</span>
     </li>
   </ul>
 </div>
-  )
 }
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  quantity: PropTypes.object.isRequired, 
+};
