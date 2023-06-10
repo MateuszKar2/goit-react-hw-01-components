466eff00d74c44de8ac28f82b98459fda1222c8d
MateuszKar2 <system505@protonmail.com>
2023-06-10 20:09:40 +0200

    add file

---
+++ src/components/Profile/Statistics.jsx
@@ -0,0 +1,24 @@
+
+
+<section class={statistics.css}>
+  <h2 class={title.css}>Upload stats</h2>
+
+  <ul class={stat-list.css}>
+    <li class={item.css}>
+      <span class={label.css}>.docx</span>
+      <span class={percentage.css}>4%</span>
+    </li>
+    <li  class={item.css}>
+      <span class={label.css}>.mp3</span>
+      <span class={percentage.css}>14%</span>
+    </li>
+    <li  class={item.css}>
+      <span class={label.css}>.pdf</span>
+      <span class={percentage.css}>41%</span>
+    </li>
+    <li  class={item.css}>
+      <span class={label.css}>.mp4</span>
+      <span class={percentage.css}>12%</span>
+    </li>
+  </ul>
+</section>
