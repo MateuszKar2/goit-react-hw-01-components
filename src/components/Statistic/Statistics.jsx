e9c2a873acae898838ae4f27630f80d62610be3e
MateuszKar2 <system505@protonmail.com>
2023-06-14 18:28:32 +0200

    add files

--- src/components/Statistic/Statistics.jsx
+++ src/components/Statistic/Statistics.jsx
@@ -1,35 +1,34 @@
 import css from './Statistic.module.css';
 import PropTypes from "prop-types";
+import { getRandomHexColor } from 'utils/getRandomHexColor';
 
 export const Statistics = ({ title, stats }) => {
-
   return (
-<section className={css.statistics}>
-  <h2 className={css.title}>{title}</h2>
-
-  <ul className={css.statlist}>
-    <li className={css.item}>
-      <span className={css.label}>.docx</span>
-      <span className={css.percentage}>4%</span>
-    </li>
-    <li  className={css.item}>
-      <span className={css.label}>.mp3</span>
-      <span className={css.percentage}>14%</span>
-    </li>
-    <li  className={css.item}>
-      <span className={css.label}>.pdf</span>
-      <span className={css.percentage}>41%</span>
-    </li>
-    <li  className={css.item}>
-      <span className={css.label}>.mp4</span>
-      <span className={css.percentage}>12%</span>
-    </li>
-  </ul>
-</section>
+      <section className={css.statistics}>
+          {title && <h2 className={css.title}>{title}</h2>}
+          <ul className={css.statList}>
+              {stats.map(el => {
+                  const listItemStyle = {
+                      backgroundColor: getRandomHexColor(),
+                  };
+                  return (
+                      <li
+                          className={css.item} style={listItemStyle} key={el.id}>
+                          <span className={css.label}>{el.label}</span>
+                          <span className={css.percentage}>{el.percentage}%</span>
+                      </li>
+                  );
+              })}
+          </ul>
+      </section>
   )
 };
 
 Statistics.propTypes = {
   title: PropTypes.string.isRequired,
-  stats: PropTypes.string.isRequired,
+  stats: PropTypes.arrayOf(PropTypes.shape({
+    key: PropTypes.string,
+    label: PropTypes.string,
+    percentage: PropTypes.number,
+  })),
 };
