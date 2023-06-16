5073a09088be6ed045b3060b1a17e8653bcc7609
MateuszKar2 <system505@protonmail.com>
2023-06-16 09:33:30 +0200

    add file

--- src/components/Statistic/Statistics.jsx
+++ src/components/Statistic/Statistics.jsx
@@ -5,7 +5,7 @@ import { getRandomHexColor } from 'utils/getRandomHexColor';
 export const Statistics = ({ title, stats }) => {
   return (
       <section className={css.Statistics}>
-          {title && <h2 className={css.title}>{title}</h2>}
+          {<h2 className={css.title}>{title}</h2>}
           <ul className={css.statList}>
               {stats.map(el => {
                   const listItemStyle = {
