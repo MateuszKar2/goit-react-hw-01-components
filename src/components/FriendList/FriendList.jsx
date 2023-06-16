b75f20d70be8bfdff75ebbeeb5be5f6370918783
MateuszKar2 <system505@protonmail.com>
2023-06-16 18:31:22 +0200

    add file

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { FriendListItem } from './FriendListItem';
 
 export const FriendList = ({ friends }) => {
+    return (
    <ul className={css.friendList}>
     {friends.map(({ avatar, name, isOnline, id }) => (
         <FriendListItem
@@ -12,8 +13,10 @@ export const FriendList = ({ friends }) => {
         key={id}
         />        
     ))}
-    </ul> 
+    </ul>
+    )
 };
+
 
 FriendList.prototype = {
     friends: PropTypes.arrayOf(PropTypes.shape({
